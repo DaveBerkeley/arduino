6c561237a8ef472af964d0e272436537f46f2be9
Dave Berkeley <git@rotwang.co.uk>
2015-03-27 13:46:29 +0000

    added initial command parser

--- sketchbook/relay_board/relay_board.ino
+++ sketchbook/relay_board/relay_board.ino
@@ -1,16 +1,98 @@
 
-// set pin numbers:
-const int relay0 = 2;
-const int relay1 = 3;
-const int relay2 = 4;
-const int relay3 = 5;
 
+// set pin numbers:
 static const int pins[4] = { 2, 3, 4, 5 };
+
+static int state[4];
 
 static void relay(int i, bool set)
 {
+  state[i] = set;
   digitalWrite(pins[i], !set);
 }
+
+  /*
+  *
+  */
+
+static int read_command(const char* cmd)
+{
+  if (*cmd != 'R')
+    return -1;
+  cmd++;
+    
+  int device = *cmd - '0';
+  if (!((device >= 0) && (device <= 4)))
+    return -1;
+  cmd++;
+
+  if (*cmd != '=')
+    return -1;
+  cmd++;
+
+  const int c = *cmd++;
+  switch (c) {
+    case '?' :  //  query
+      break;
+      
+    case 'P' :
+      // pulse
+      break;
+      
+    case 'T' :  //  toggle
+      if (*cmd != '\0')
+        return -1;
+      relay(device, !state[device]);
+      break;
+      
+    case '0' :  //  off
+    case '1' :  //  on
+      if (*cmd != '\0')
+        return -1;
+      relay(device, c - '0');
+      break;
+      
+    default  :
+      return -1;
+  }
+
+  Serial.print("R");
+  Serial.print(device);
+  Serial.print("=");
+  Serial.print(state[device]);
+  Serial.print("\n");
+}
+
+static int get_serial()
+{
+  static char buff[16];
+  static int idx = 0;
+
+  if (!Serial.available())
+    return -1;
+
+  const int c = Serial.read();
+
+  buff[idx++] = c;
+  if (idx >= (sizeof(buff)-1)) {
+    // too many chars
+    idx = 0;
+    return -1;
+  }
+
+  // read a line
+  if ((c != '\r') && (c != '\n'))
+    return -1;
+
+  // null out the \n
+  buff[idx-1] = '\0';
+  idx = 0;
+  return read_command(buff);
+}
+
+  /*
+  *
+  */
 
 void setup()
 {
@@ -24,29 +106,7 @@ void setup()
 
 void loop()
 {
-  static int i = 0;
-  static int j = 0;
-  
-  if (++i < 10000)
-    return;
-  i = 0;
-  if (++j < 50)
-    return;
-  j = 0;
-  
-  static int shift = 1;
-
-  Serial.print(shift);
-  Serial.print("\n");
-
-  relay(0, shift & 0x01);
-  relay(1, shift & 0x02);
-  relay(2, shift & 0x04);
-  relay(3, shift & 0x08);
-
-  shift <<= 1;
-  if (shift == 0x10)
-    shift = 0x01;
+  get_serial();
 }
 
 // FIN
