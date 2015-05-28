3591661e383338f171a532073ae509263624d925
Dave Berkeley <dave@rotwang.co.uk>
2015-05-28 06:49:44 +0100

    added software serial debugging

--- sketchbook/test84/test84.ino
+++ sketchbook/test84/test84.ino
@@ -1,9 +1,14 @@
 
-#define PIN 10
+
+#include <SoftwareSerial.h>
+
+#define rx 0
+#define tx 1
+SoftwareSerial serial(rx, tx);
 
 static char pins[] = {
-  0, 
-  1,
+  //0, 
+  //1,
   2,
   3, 
   7,  
@@ -12,19 +17,19 @@ static char pins[] = {
 };
 
 void setup() {
-  // put your setup code here, to run once:
+
   for (int i = 0; ; i++)
   {
     if (pins[i] == -1)
       break;
     pinMode(pins[i], OUTPUT);    
   }
+
+  serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  delay(10);
+  delay(100);
 
   static int state;
   for (int i = 0; ; i++) {
@@ -33,4 +38,6 @@ void loop() {
     digitalWrite(pins[i], (state & (1 << i)) ? HIGH : LOW);
   }
   state += 1;
+  serial.print(state);
+  serial.print("\r\n");
 }
