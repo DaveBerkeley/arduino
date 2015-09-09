5ac4aa8997fb8e5c059e8975019922495c92f743
Dave Berkeley <dave@rotwang.co.uk>
2015-09-09 12:16:56 +0100

    added radio code

--- sketchbook/magnetometer/magnetometer.ino
+++ sketchbook/magnetometer/magnetometer.ino
@@ -1,7 +1,24 @@
 
+/*
+ Copyright (C) 2015 Dave Berkeley projects2@rotwang.co.uk
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+ USA
+*/
+
 /* 
- * Dave Berkeley (c) 2015 projects2@rotwang.co.uk
- * 
  * Based on Compass5883JeeNodes.ino
  * code for the Modern Device HMC5883 Compass sensor used with JeeNodes
  * Requires the JeeLib library found here: https://github.com/jcw/jeelib
@@ -9,7 +26,20 @@
  */
 
 #include <JeeLib.h>
-#include <math.h>
+
+#include <radiodev.h>
+#include <radioutils.h>
+
+  /*
+   *
+   */
+
+// needed by the watchdog code
+EMPTY_INTERRUPT(WDT_vect);
+
+  /*
+   *  HMC5883 3-axis Magnetometer
+   */
 
 #define HMC5883_WriteAddress 0x1E //  i.e 0x3C >> 1
 #define HMC5883_ModeRegisterAddress 0x02
@@ -19,8 +49,12 @@
 #define regb     0x01
 #define regbdata  0x40
 
+  /*
+   *
+   */
+
 class Magnetometer : public DeviceI2C {
-    int data[6];
+    int16_t data[6];
     PortI2C& m_port;
 
 public:
@@ -65,6 +99,62 @@ PortI2C myBus (JEEPORT);
 
 Magnetometer magnetometer(myBus);
 
+// node -> gateway data
+#define PRESENT_TEMPERATURE (1 << 1)
+#define PRESENT_VCC         (1 << 2)
+#define PRESENT_X           (1 << 3)
+#define PRESENT_Y           (1 << 4)
+#define PRESENT_Z           (1 << 5)
+
+  /*
+  *
+  */
+
+class MagnetometerRadio : public RadioDev
+{
+public:
+
+  MagnetometerRadio()
+  : RadioDev(GATEWAY_ID, 1)
+  {
+  }
+
+  virtual void init()
+  {
+    RadioDev::init();
+
+    // use the 1.1V internal ref for the ADC
+    analogReference(INTERNAL);
+    
+    //pinMode(PULLUP_PIN, INPUT_PULLUP);
+  }
+
+  virtual const char* banner()
+  {
+    return "Magnetometer v1.0";
+  }
+
+  virtual void append_message(Message* msg)
+  {
+    const uint16_t vcc = read_vcc();
+    msg->append(PRESENT_VCC, & vcc, sizeof(vcc));
+    
+    int16_t x, y, z;
+    magnetometer.convert(& x, & y, &z);
+
+    msg->append(PRESENT_X, & x, sizeof(x));
+    msg->append(PRESENT_Y, & y, sizeof(y));
+    msg->append(PRESENT_Z, & z, sizeof(z));
+  }
+
+  virtual void loop(void)
+  {
+    radio_loop(32767);
+  }
+};
+
+static MagnetometerRadio radio;
+
   /*
   *
   */
@@ -77,20 +167,12 @@ void setup () {
         Serial.print(JEEPORT);
         Serial.print("\n");
     }
+    
+    radio.init();
 }
 
 void loop() {
-    int16_t x,y,z;
-    magnetometer.convert(& x, &y, &z);
-
-    Serial.print(x);
-    Serial.print(" ");
-    Serial.print(y);
-    Serial.print(" ");
-    Serial.print(z);
-    Serial.print("\n");
-
-    delay(500);
+    radio.loop();
 }
 
 // FIN
