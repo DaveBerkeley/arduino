24e4a09622635978c5673e5c6ff255f33b707531
Dave Berkeley <dave@rotwang.co.uk>
2015-11-13 17:01:13 +0000

    add known working optiboot hex file

--- boot/optiboot/burn.sh
+++ boot/optiboot/burn.sh
@@ -2,6 +2,11 @@
 
 DUDE_ARGS='-p atmega328p -c stk500v1 -P /dev/ttyUSB1 -b 19200'
 
-avrdude $DUDE_ARGS -v
+#avrdude $DUDE_ARGS -v
+
+avrdude $DUDE_ARGS -e -Ulock:w:0x3F:m -Uefuse:w:0x05:m -Uhfuse:w:0xde:m -Ulfuse:w:0xff:m
+
+#avrdude $DUDE_ARGS -Uflash:w:optiboot_atmega328.hex:i -Ulock:w:0x0F:m
+avrdude $DUDE_ARGS -Uflash:w:std_optiboot_atmega328.hex:i -Ulock:w:0x0F:m
 
 # FIN
