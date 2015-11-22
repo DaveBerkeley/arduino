ad6caf828268392dccaea374af2ad5a101c32dd9
Dave Berkeley <dave@rotwang.co.uk>
2015-11-22 20:55:43 +0000

    strip blink down to minimum

--- sketchbook/blink/blink.ino
+++ sketchbook/blink/blink.ino
@@ -1,58 +1,11 @@
 
-//#define FLASH
-
-#if defined(FLASH)
-
-#include <flash.h>
-
-    /*
-     *  I2C Interface.
-     */
-
-#define TRIGGER
-
-static PinIo d4 = PinIoD(4);
-static PinIo a0 = PinIoC(0);
-
-#if defined(TRIGGER)
-static PinIo d6 = PinIoD(6);
-
-static void trig()
-{
-    // trigger pulse for logic analyser
-    pin_mode(& d6, true);
-    pin_set(& d6, false);
-    pin_set(& d6, true);
-}
-#endif
-
-static I2C i2c = {
-    & d4,   //  SDA
-    & a0,   //  SCL
-    0x50 << 1,
-    0, // i2c_delay,
-#if defined(TRIGGER)
-    & trig,
-#endif
-};
-
-static FlashIO io = {
-    & i2c,
-};
-
-#endif // defined(FLASH)
-
-#define LED 4
+#define LED 6
 
 void setup()
 {
   Serial.begin(57600);
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
-
-#if defined(FLASH)
-  i2c_init(& i2c);
-#endif
 }
 
 void loop()
@@ -63,7 +16,5 @@ void loop()
   on = !on;
   delay(200);
 
-#if defined(FLASH)
-  Serial.print(i2c_is_present(& i2c));
-#endif
+  Serial.print(".");
 }
